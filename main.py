ed52a5652eaf5d8a5dbfd95e0afd90dc371136c3
Nicol Carstens <nicol.carstens@bdrthermea.com>
2025-03-20 00:07:31 +0100

    CH2_L2: Game Loop

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((50, 20, 40))
+        pygame.display.flip()
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
