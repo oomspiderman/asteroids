d9784a330a4ad8ad7a9c5035359a5afd8ff95167
Nicol Carstens <oomspiderman@gmail.com>
2025-05-26 18:31:40 +0200

    Use provided solution

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 
+
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    running = True
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((50, 20, 40))
+        screen.fill("black")
         pygame.display.flip()
 
-    pygame.quit()
 
 if __name__ == "__main__":
     main()
