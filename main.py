7a5483f8ae7710ef3194f3f20b7920478abbf09c
Nicol Carstens <oomspiderman@gmail.com>
2025-06-07 21:26:58 +0200

    Add triangular player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -9,12 +10,17 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    p1 = Player(x,y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        p1.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
