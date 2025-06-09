e2ef0337a3567adea49320998fcf605ec3db0e0c
Nicol Carstens <oomspiderman@gmail.com>
2025-06-09 10:39:42 +0200

    Introduce Containers and Groups

--- main.py
+++ main.py
@@ -5,23 +5,31 @@ from player import *
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    p1 = Player(x,y)
+    updatable_group = pygame.sprite.Group()
+    drawable_group  = pygame.sprite.Group()
+
+    Player.containers = (updatable_group,drawable_group)
+
+    p1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        p1.update(dt)
+        for player in updatable_group:
+            player.update(dt)
+
         screen.fill("black")
-        p1.draw(screen)
+
+        for player in updatable_group:
+            player.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
